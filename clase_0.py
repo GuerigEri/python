e5f910a12f2c94724e4f313dd462316c0b123204
Guerig Eri <guerig.cj@gmail.com>
2026-05-25 04:14:30 +0200

    Intro
    Variables y valores númericos

--- clase_0.py
+++ clase_0.py
@@ -3,6 +3,44 @@ print("Hola mundo");
 # comentario
 '''
 bloque de comentario
+
+Esto no se muestra por terminal
 '''
 
+# Variables
 
+anios = "tengo "
+
+edad = 25
+
+print(f"{anios}{edad}")
+
+
+mensaje_tinta = "Tienes un '20' de tinta en la impresora"
+porcentaje = "100%"
+
+print(mensaje_tinta)
+print("Tienes un " + porcentaje + " de tinta en la impresora")
+
+
+## Valores numericos -------------
+
+# Integer
+numero = 5
+
+# float
+altura = 1.75
+
+# double
+peso = 50.5
+
+# Igualdad
+
+print(5 == 5)
+
+# Diferente o distinto que
+print (5 != 5)
+
+# Mayor que / menor que
+print( 10 > 3)
+print(10 < 3)
